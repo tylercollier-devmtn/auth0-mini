14129de7cbcac4afe5b5419dd1513cef8f1c2b34
Tyler Collier <tyler.collier+github@devmounta.in>
2018-05-14 11:35:10 -0700

    Fix bug where username/password could not be used

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
 
   login() {
     const redirectUri = encodeURIComponent(`${window.location.origin}/auth/callback`);
-    window.location = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/login?client=${process.env.REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${redirectUri}`
+    window.location = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${process.env.REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${redirectUri}&response_type=code`
   }
 
   logout() {
